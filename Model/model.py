e366a31c1ba8b1909f418d71b0ae77db3ec1141e
HumanStewie <mopro1010@gmail.com>
2025-10-04 12:28:50 +0700

    Current ARIMA model (should function fine) - Havent tested accuracy

--- Model/model.py
+++ Model/model.py
@@ -9,13 +9,12 @@ from statsmodels.tsa.stattools import adfuller
 
 
 class ARIMAModel:
-    def __init__(self, order : tuple):
-        self.order = order
+    def __init__(self, data : pd.DataFrame):
         self.model = None
         self.result = None
-        self.data = None
+        self.data = data
 
-    def best_order(self, data, d_val=0, max_d=2) -> list:
+    def best_order(self, d_val=0, max_d=2) -> tuple:
         """
         :param data: pd.Series
         :param d_val: current differencing count
@@ -23,21 +22,28 @@ class ARIMAModel:
         :return: optimal d (0, 1, 2)
         """
         # data will be a DataFrame by pandas
-        check = adfuller(data.dropna()) # dropna() to remove NaN values
+        check = adfuller(self.data.dropna()) # dropna() to remove NaN values
         p_value = check[1]
 
         # 0.05 is our base
         if p_value <= 0.05 or d_val == max_d:
             # if stationary or reached max differencing
-            order_result = arma_order_select_ic(data.dropna(), ic='aic', trend='n')
+            order_result : dict = arma_order_select_ic(self.data.dropna(), ic='aic', trend='n')
             best_aic : list = order_result.aic_min_order
 
             best_p = best_aic[0]
             best_q = best_aic[1]
-
-            return best_p, d_val, best_q
+            return (best_p, d_val, best_q)
         
         else:
             # if not stationary:
-            data_diff = data.diff().dropna()
+            data_diff = self.data.diff().dropna()
             return self.check_stationary(data_diff, d_val + 1)
+        
+    def fit(self, data) -> None:
+        self.model = ARIMA(data, order=self.best_order(data))
+        self.result = self.model.fit()
+
+    def forecast(self, steps : int = 10, ) -> np.ndarray | pd.DataFrame | pd.Series:
+        forecast_array = self.result.forecast(steps=steps)
+        return forecast_array
