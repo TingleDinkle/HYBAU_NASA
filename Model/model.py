ace689368f758a9f65515836bcad12704359d8ed
HumanStewie <mopro1010@gmail.com>
2025-10-04 14:16:46 +0700

    added docstring + type hints, changed default steps to 3

--- Model/model.py
+++ Model/model.py
@@ -12,7 +12,7 @@ class ARIMAModel:
         self.result = None
         self.data = data
 
-    def best_order(self, data, d_val=0, max_d=2) -> tuple:
+    def best_order(self, data : pd.DataFrame, d_val : int = 0, max_d : int = 2) -> tuple:
         """
         :param data: pd.Series
         :param d_val: current differencing count
@@ -26,6 +26,7 @@ class ARIMAModel:
         # 0.05 is our base
         if p_value <= 0.05 or d_val == max_d:
             # if stationary or reached max differencing
+            # Auto create best orders for ARIMA
             order_result : dict = arma_order_select_ic(data.dropna(), ic='aic', trend='n')
             best_aic : list = order_result.aic_min_order
 
@@ -41,11 +42,16 @@ class ARIMAModel:
     def fit(self) -> None:
         """
         :param data: pd.DataFrame
+        :return: Doesn't return anything but it sets result attribute to the fitted model
         """
         self.model = ARIMA(self.data, order=self.best_order(self.data))
         # method_kawrgs to increase maximum number of iterations (Resolve ConvergenceWarnings)
         self.result = self.model.fit(method_kwargs={'maxiter':300})
 
-    def forecast(self, steps : int = 10, ) -> np.ndarray:
+    def forecast(self, steps : int = 3, ) -> np.ndarray:
+        """
+        :param steps: number of steps to forecast (in this case, days)
+        :return: np.ndarray of forecast obj.
+        """
         forecast_obj = self.result.get_forecast(steps=steps)
         return forecast_obj
