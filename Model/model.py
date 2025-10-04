cfa0a49cd785f05c2859fdeeef750ef463b55975
b3017h <b3017h@gmail.com>
2025-10-04 12:02:18 +0700

    Small fix for model.py: Added actual differencing per recursion for check_stationary; added max_d=2 (default val), added d_val=0 (default val)

--- Model/model.py
+++ Model/model.py
@@ -15,17 +15,22 @@ class ARIMAModel:
         self.result = None
         self.data = None
 
-    def check_stationary(self, data, d_val) -> int:
-        
+    def check_stationary(self, data, d_val=0, max_d=2) -> int:
+        """
+        :param data: pd.Series
+        :param d_val: current differencing count
+        :param max_d: maximum differencing (default = 2)
+        :return: optimal d (0, 1, 2)
+        """
         # data will be a DataFrame by pandas
         check = adfuller(data.dropna()) # dropna() to remove NaN values
         p_value = check[1]
 
         # 0.05 is our base
-        if p_value <= 0.05:
-            # return order d = 0 (already stationary)
+        if p_value <= 0.05 or d_val == max_d:
             return d_val
-        else: 
-            # d = 1 (make stationary)
-            return self.check_stationary(data, d_val + 1)
+        else:
+            # if not stationary:
+            data_diff = data.diff().dropna()
+            return self.check_stationary(data_diff, d_val + 1)
     
