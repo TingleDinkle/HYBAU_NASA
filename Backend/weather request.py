b85acfd2b3b9e8171d6b7642c52ba69e2a8c385a
SherlockWalker <140088079+SherlockWalker@users.noreply.github.com>
2025-10-04 15:24:32 +0700

    The working version of getting 6 types of data (temp, wind speed/direction, rain, clouds, pressure, humidity) from OpenMeteo

--- Backend/weather request.py
+++ Backend/weather request.py
@@ -1,43 +1,40 @@
-# Data needs to be formatted. For now, this should be good
+# UNFORMATTED DATA, NOT THE FINAL VERSION. For now, this should be good
 # Duration of forecast should also matter
 
+from pprint import pprint
 import requests
+import datetime
 
-def weather_mateo(lat, lon):
+def weather_meteo(lat, lon, days=5, timezone="auto"):
+    # Determine date range
+    end_date = datetime.date.today() #today (UTC)
+    start_date = end_date - datetime.timedelta(days=days)
+    
     base = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": lat,
         "longitude": lon,
-        "hourly": "temperature_2m,relative_humidity_2m,precipitation",
-        "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum",
-        "timezone": "auto",
-        "forecast_days": 1
+        "hourly": ",".join([
+            "temperature_2m",
+            "wind_speed_10m",
+            "wind_direction_10m",
+            "precipitation"
+            "cloud_cover",
+            "surface_pressure",
+            "relative_humidity_2m"
+            ]),
+        "start_date": start_date.isoformat(),
+        "end_date": end_date.isoformat(),
+        "timezone": timezone
     }
     recall = requests.get(base, params=params)
     recall.raise_for_status()
     return recall.json()
-
-
-def weather_google(api_key, lat, lon, days=5):
-    base = "https://weather.googleapis.com/v1/forecast/days:lookup"
-    params = {
-        "key": api_key,
-        "location.latitude": lat,
-        "location.longitude": lon,
-        "days": days
-    }
-    recall = requests.get(base, params=params)
-    recall.raise_for_status()
-    return recall.json()
-
-key = "" #I FORGOT TO GET A KEY
 
 if __name__ == "__main__":
-    latitude, longtitude = 21.0278, 105.8342
+    latitude, longtitude = 21.0278, 105.8342 #Swap this out later to fit with front end
     
-    data = [weather_mateo(latitude, longtitude),
-            weather_google(key, latitude, longtitude, 1)
-            ]
+    data = [weather_meteo(latitude, longtitude)]
     for x in data:
-        print(x)
+        pprint(x)
         print("\n")
