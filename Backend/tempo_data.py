bc9304c0d9e82303aae7cef0e6e330b4cf1bc5fa
HumanStewie <mopro1010@gmail.com>
2025-10-05 12:28:35 +0700

    Added docstring and comments to TEMPO

--- Backend/tempo_data.py
+++ Backend/tempo_data.py
@@ -4,13 +4,24 @@ import datetime
 import xarray as xa
 
 class TempoData:
-    def __init__(self, chem : str, lon : float, lat : float): # initiate login procedure
+    def __init__(self, chem : str, lon : float, lat : float): 
+        """
+        Initiate login procedures, longitude and latitude (in that order)
+        :param chem: Chemical to search for (NO2, O3TOT, HCHO)
+        :param lon: Longitude of location
+        :param lat: Latitude of location
+        """
         self.chem = chem
         self.login = earthaccess.login(persist=True)
         self.lon = lon
         self.lat = lat
 
-    def search_data(self) -> list: # search for specified chemical
+    def search_data(self) -> list:
+        """
+        Search for .nc files
+        :param None
+        :return: A List[DataGranules] of search results
+        """
         # Get time range (16 hours ago from now)
         today : datetime = datetime.datetime.now()
         three_hours_ago : datetime = datetime.timedelta(hours=16)
@@ -23,7 +34,12 @@ class TempoData:
                                 count=1)
         return result
 
-    def fetch_data(self, results : list): # averages the entire plane of heatmap to an average num.
+    def fetch_data(self, results : list):
+        """
+        Get the average of heatmap data and turn it into a float.
+        :param results: A List[DataGranules] of search results
+        :return: A float of the mean value of the chemical
+        """
         # result[0] for products
         opn : xa.Dataset = earthaccess.open_virtual_dataset(results[0], group="product")
         
@@ -45,13 +61,14 @@ class TempoData:
         return num
 
 if __name__ == "__main__":
+    # Test data
     tempo = TempoData("NO2", -82.370971, 36.146747)
-    no2 = tempo.fetch_data(tempo.search_data())
+    no2 = tempo.fetch_data(tempo.search_data()) # 10/5 9.915e+14
 
     tempo = TempoData("O3TOT", -82.370971, 36.146747)
-    o3 = tempo.fetch_data(tempo.search_data())
+    o3 = tempo.fetch_data(tempo.search_data()) # 10/5 2.689e+02
 
     tempo = TempoData("HCHO", -82.370971, 36.146747)
-    hcho = tempo.fetch_data(tempo.search_data())
+    hcho = tempo.fetch_data(tempo.search_data()) # 10/5 8.281e+15
 
     print(no2, o3, hcho, end="\n")
